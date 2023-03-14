c66f48c7f2e411075d7c78a9ceac818c23f0ae1a
sophiamziou <sofiamziou@gmail.com>
2023-03-14 20:18:02 +0100

    show route for api done

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -16,4 +16,21 @@ class ProjectController extends Controller
             'results' => $projects,
         ]);
     }
+    public function show($slug)
+    {
+        $project = Project::all()->where('slug', $slug)->first();
+
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun post trovato'
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{slug}', [ProjectController::class, 'show']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
